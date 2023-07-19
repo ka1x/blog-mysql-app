3c5aa924f6dbf2618cd3dda0d1e9ddeadf2cd359
ka1x <kath021@interia.pl>
2023-07-20 00:13:43 +0200

    post styling

---
+++ client/README.md
@@ -0,0 +1,2 @@
+https://www.youtube.com/watch?v=0aPLk2e2Z3g&list=WL
+https://github.com/safak/youtube2022/tree/blog-app
--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import {categories, posts} from '../../data/index.js';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './menu.scss';
 
 const Menu = ({cat}) => {
    return (
       <div className='menu'>
          <h1>Other posts you may like</h1>
-         {posts.map((post) => (
+         {posts.slice(0, 4).map((post) => (
             <div
                className='post'
                key={post.id}>
-               <img
-                  src={`../upload/${post?.img}`}
-                  alt=''
-               />
-               <h2>{post.title}</h2>
-               <Link
-                  className='link'
-                  to={`/post/${post.id}`}>
-                  <button>Read More</button>
-               </Link>{' '}
+               <div className='img-container'>
+                  <img
+                     // src={`../upload/${post?.img}`}
+                     src={`${post?.img}`}
+                     alt=''
+                  />
+               </div>
+               <div className='title-container'>
+                  <h2>{post.title}</h2>
+                  <Link
+                     className='link'
+                     to={`/post/${post.id}`}>
+                     <div className='btn-container'>
+                        <button>Read More</button>
+                        {/* <i className='ri-arrow-right-s-line'></i> */}
+                     </div>
+                  </Link>
+               </div>
             </div>
          ))}
       </div>
