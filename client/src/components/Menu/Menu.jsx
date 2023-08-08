8b41e10a2dbe5caab597bd78e9ae85d57f87021a
ka1x <kath021@interia.pl>
2023-08-08 16:27:08 +0200

    fix menu width issue

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -28,19 +28,17 @@ const Menu = ({cat}) => {
                key={post.id}>
                <div className='img-container'>
                   <img
-                     // src={`../upload/${post?.img}`}
                      src={`/uploads/${post?.img}`}
                      alt=''
                   />
                </div>
                <div className='title-container'>
-                  <h2>{post.title}</h2>
+                  <h2>{post?.title}</h2>
                   <Link
                      className='link'
                      to={`/post/${post.id}`}>
                      <div className='btn-container'>
                         <button>Read More</button>
-                        {/* <i className='ri-arrow-right-s-line'></i> */}
                      </div>
                   </Link>
                </div>
