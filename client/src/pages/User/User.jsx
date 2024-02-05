27679d58e70fec0516a28accfa0985f7b8169b77
ka1x <kath021@interia.pl>
2024-02-05 23:15:50 +0100

    user page html

--- client/src/pages/User/User.jsx
+++ client/src/pages/User/User.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {Link, useLocation} from 'react-router-dom';
+import {useAuth} from '../../context/AuthContext';
+import './user.scss';
+import {Loading, Navbar, Menu} from '../../components';
+import axios from 'axios';
+import {getText} from '../../utils/getText';
 
 const User = () => {
-  return (
-	 <div>User</div>
-  )
-}
+   const [loading, setLoading] = useState(true);
+   const {currentUser} = useAuth();
 
-export default User
+   const [user, setUser] = useState(null);
+   const [userPosts, setUserPosts] = useState(null);
+
+   const location = useLocation();
+   const profileId = location.pathname.split('/')[2];
+
+
+	
+   return (
+      <>
+         {loading ? (
+            <Loading />
+         ) : (
+            <>
+               <Navbar />
+
+               <div className='profile-container'>
+                  <div className='sidebar'>
+                     <h3>Profile</h3>
+                     <div className='user-pfp'>
+                        <img
+                           src={user?.img ? `/uploads/${user?.img}` : '/user-circle-svgrepo-com.png'}
+                           alt=''
+                        />
+                     </div>
+                     <div className='name-container'>
+                        <p style={{textTransform: 'uppercase'}}>{` ${user?.firstname} ${user?.lastname}`}</p>
+                     </div>
+                     {user?.id === currentUser.id && (
+                        <>
+                           <Link
+                              to={'/options'}
+                              state={user}>
+                              <button className='options-btn'>Options</button>
+                           </Link>
+                        </>
+                     )}
+                  </div>
+
+                  <div className='user-content'>
+                     <h3 className='page-title'> Latest posts </h3>
+                     <div className='posts'>
+                        {userPosts?.map((post, i) => (
+                           <Link
+                              to={`/post/${post.id}`}
+                              className='post'
+                              key={i}>
+                              <div className='img'>
+                                 <img
+                                    src={post.img ? `/uploads/${post.img}` : 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'}
+                                    alt=''
+                                 />
+                              </div>
+                              <div className='content'>
+                                 <div className='text'>
+                                    <h3>{post?.title}</h3>
+                                    <p className='desc'>{getText(post?.desc, 50)}</p>
+                                 </div>
+                              </div>
+                           </Link>
+                        ))}
+                     </div>
+                  </div>
+               </div>
+            </>
+         )}
+      </>
+   );
+};
+
+export default User;
