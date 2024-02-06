fbd8651d052340626f3dc77ad14e58e8bb81a8f1
ka1x <kath021@interia.pl>
2024-02-06 02:25:47 +0100

    style cleanup

--- client/src/pages/Post/Post.jsx
+++ client/src/pages/Post/Post.jsx
@@ -37,7 +37,7 @@ const Post = () => {
    useEffect(() => {
       const timeoutId = setTimeout(() => {
          setAnimate(false);
-      }, 800); // Slightly shorter timeout than aimation duration
+      }, 800);
 
       return () => clearTimeout(timeoutId);
    }, [postId]);
