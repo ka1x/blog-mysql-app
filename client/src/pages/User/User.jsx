57309e8dc656772534f5b893b60d8da5410bdc4a
ka1x <kath021@interia.pl>
2024-02-06 02:10:58 +0100

    meida queries adjustments

--- client/src/pages/User/User.jsx
+++ client/src/pages/User/User.jsx
@@ -74,28 +74,37 @@ const User = () => {
                      </div>
 
                      <div className='user-content'>
-                        <h3 className='page-title'> Latest posts </h3>
-                        <div className='posts'>
-                           {userPosts?.map((post, i) => (
-                              <Link
-                                 to={`/post/${post.id}`}
-                                 className='post'
-                                 key={i}>
-                                 <div className='img'>
-                                    <img
-                                       src={post.img ? `/uploads/${post.img}` : 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'}
-                                       alt=''
-                                    />
-                                 </div>
-                                 <div className='content'>
-                                    <div className='text'>
-                                       <h3>{post?.title}</h3>
-                                       <p className='desc'>{getText(post?.desc, 50)}</p>
-                                    </div>
-                                 </div>
-                              </Link>
-                           ))}
-                        </div>
+                        {userPosts != 0 ? (
+                           <>
+                              <h3 className='page-title'> Latest posts </h3>
+
+                              <div className='posts'>
+                                 {userPosts?.map((post, i) => (
+                                    <Link
+                                       to={`/post/${post.id}`}
+                                       className='post'
+                                       key={i}>
+                                       <div className='img'>
+                                          <img
+                                             src={post.img ? `/uploads/${post.img}` : 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'}
+                                             alt=''
+                                          />
+                                       </div>
+                                       <div className='content'>
+                                          <div className='text'>
+                                             <h3>{post?.title}</h3>
+                                             <p className='desc'>{getText(post?.desc, 50)}</p>
+                                          </div>
+                                       </div>
+                                    </Link>
+                                 ))}
+                              </div>
+                           </>
+                        ) : (
+                           <>
+                              <EmptyPageMsg message={'User has not posted any content'} />
+                           </>
+                        )}
                      </div>
                   </div>
                ) : (
