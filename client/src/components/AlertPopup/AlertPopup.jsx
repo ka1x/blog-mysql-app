b90985e7e45510b59dce05d5e3fcd13764466c0e
ka1x <kath021@interia.pl>
2024-02-06 02:31:20 +0100

    visual adjustment

--- client/src/components/AlertPopup/AlertPopup.jsx
+++ client/src/components/AlertPopup/AlertPopup.jsx
@@ -7,8 +7,10 @@ const AlertPopup = ({message, onConfirm, onCancel, showCancel}) => {
          <div className='overlay'></div>
          <div className='popup-content'>
             <p>{message}</p>
-            <button onClick={onConfirm}>Confirm</button>
-            {showCancel == true && <button onClick={onCancel}>Cancel</button>}
+            <div>
+               <button onClick={onConfirm}>Confirm</button>
+               {showCancel == true && <button onClick={onCancel}>Cancel</button>}
+            </div>
          </div>
       </div>
    );
