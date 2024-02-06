b75def4dd501e7484ae39b5e14cb4e069ece255c
ka1x <kath021@interia.pl>
2024-02-06 23:20:52 +0100

    intrgration with railway

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
-import mysql from 'mysql2'
+import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'blog'
-})
+   host: 'monorail.proxy.rlwy.net',
+   user: 'root',
+   password: '6AebcBddfD3fEafbgDAAgEfGFee153CB',
+   database: 'railway',
+});
