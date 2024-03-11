9a1ec0777a5bb2f7dd6d62fb47a538e33c019b09
ka1x <kath021@interia.pl>
2024-03-11 20:08:07 +0100

    db disconnect fix

--- server/db.js
+++ server/db.js
@@ -1,11 +1,54 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv'
-dotenv.config()
+// import mysql from 'mysql2';
+// import dotenv from 'dotenv'
+// dotenv.config()
 
-export const db = mysql.createConnection({
-   host: 'monorail.proxy.rlwy.net',
-   user: 'root',
-   password: process.env.DB_PASS,
-   database: 'railway',
-   port : '25634',
-});
+// export const db = mysql.createConnection({
+//    host: 'monorail.proxy.rlwy.net',
+//    user: 'root',
+//    password: process.env.DB_PASS,
+//    database: 'railway',
+//    port : '25634',
+// });
+
+
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+let db;
+
+function createConnection() {
+    db = mysql.createConnection({
+        host: 'monorail.proxy.rlwy.net',
+        user: 'root',
+        password: process.env.DB_PASS,
+        database: 'railway',
+        port: '25634',
+    });
+
+    db.connect(function (err) {
+        if (err) {
+            console.log("error when connecting to db:", err);
+            setTimeout(handleDisconnect, 2000);
+        } else {
+            console.log("connection is successful");
+        }
+    });
+
+    db.on("error", function (err) {
+        console.log("db error", err);
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+function handleDisconnect() {
+    createConnection();
+}
+
+createConnection();
+
+export { db };
